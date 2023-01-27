ad4368ec90c8f8fd83b91d9dfd27255d8a61094a
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2023-01-27 02:25:14 -0600

    update tests

--- services/modules/hello_world/src/main.rs
+++ services/modules/hello_world/src/main.rs
@@ -13,16 +13,14 @@ pub fn greeting(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
-
-
 #[cfg(test)]
 mod tests {
 
     use marine_rs_sdk_test::marine_test;
     
     #[marine_test(
-        config_path = "../../.fluence/tmp/Config.toml",
-        modules_dir = "target/wasm32-wasi/release/"
+        config_path = "../../../.fluence/tmp/Config.toml",
+        modules_dir = "../../services/modules/hello_world/target/wasm32-wasi/release/"
     )]
     fn test_hello_world(greeter: marine_test_env::hello_world::ModuleInterface) {
         let res = greeter.hello_world();
